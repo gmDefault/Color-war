6ce81326131e7f565d06ccdf8f08f5e8dc9b755a
Julien <julien.courtial@laposte.net>
2017-06-06 17:40:11 +0200

    parser a debuger

--- src/machine_a_glace/Comportement.java
+++ src/machine_a_glace/Comportement.java
@@ -1,14 +1,18 @@
 package machine_a_glace;
 
 public enum Comportement implements Expr {
-	Explore("X"), Attack("A");
+	Explore("X"), Attack("A"), Protect("P"), Kamikaze("K");
 
 	String affichage;
 
 	private Comportement(String s) {
 		affichage = s;
 	}
-
+	
+	public String toString(){
+		return affichage;
+	}
+	
 	public boolean isComportement() {
 		return true;
 	}
