94d035b37ef215b5ee178bd299a00f8d599ef00c
HugoGDA <gdahugo@gmail.com>
2017-06-09 12:53:51 +0200

    Revert "lastversion"
    This reverts commit fa8b30b8f511e26cbd425e06c6eee56b40bc91b5.

--- maps/map/collision.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset name="collision" tilewidth="32" tileheight="32" tilecount="25">
- <image source="collision.png" width="160" height="160"/>
-</tileset>
